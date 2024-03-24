68257b1642fc841b2e9300960add41c0138a6a20
Esprinct <164550434+Esprinct@users.noreply.github.com>
2024-03-24 23:08:38 +0100

    Add files via upload

---
+++ Afficheur_image_nul/AffichageImage.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+
+def affichageimage():
+    fenetre2 = Toplevel()
+    fenetre2.title("fenetre")
+    fenetre2.geometry("1076x1080")
+    fenetre2.minsize(480, 360)
+    fenetre2.config(background='white')
+    #Label de l'image
+    label_title = Label(fenetre2, text="SARKOSY EN 3D AVEC UNE CHAINE EN OR QUI BRILLE", font =("Helvetica", 25), bg =('white'))
+    label_title.pack(expand=YES)
+    #image de culture
+    hauteur = 635
+    Largeur = 411
+    nude = ImageTk.PhotoImage(Image.open("image.png"))
+    #canvas
+    canvas = Canvas(fenetre2,width=hauteur, height= Largeur)
+    canvas.create_image(hauteur/2,Largeur/2, image=nude)
+    canvas.pack(expand='YES')
+
+
+    fenetre2.mainloop()
---
+++ Afficheur_image_nul/Main.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+import webbrowser
+import AffichageImage 
+# Fonctions contenant ce que font les deux boutons
+
+# bouton1
+
+def lien():
+    webbrowser.open_new("https://twitter.com/esprinct")
+
+# bouton2
+
+def lien2():
+    AffichageImage.affichageimage()
+
+# Création de la fenetre
+    
+fenetre = Tk()
+fenetre.title("fenetre")
+fenetre.geometry("720x480")
+fenetre.minsize(480, 360)
+fenetre.config(background='#41B77F')
+
+#Affichage de la boite
+boite = Frame(fenetre, bg ='gray', bd=1, relief=SUNKEN)
+boite.pack(expand=YES)
+
+# Création du Texte
+# Affichage du Titre
+label_title = Label(boite, text="Bonjour", font =("Helvetica", 15), bg =('gray'))
+label_title.pack(expand=YES)
+
+# Affichage du Sous-titre
+label_subtitle = Label(boite, text="Souhaitez-vous accéder à la CULTURE?", font =("Helvetica", 12), bg =('grey'))
+label_subtitle.pack(expand=YES)
+
+# Affichage du Bouton 1
+boutton= Button(boite, text="Oui, je souhaite plus de culture.", font =("Yada Yada Yada", 15), bg =('white'),command=lien)
+boutton.pack(pady=25, fill=X)
+
+#Affivhage du Bouton2
+boutton2= Button(boite, text="Oui, je veux ENCORE plus de culture", font =("Yada Yada Yada", 15), bg =('white'),command=lien2)
+boutton2.pack(pady=25, fill=X)
+
+fenetre.mainloop()
